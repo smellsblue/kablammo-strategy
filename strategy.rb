5bf546f4104278bc1e4c8a1fb995ae43add7a63f
Mike Virata-Stone <mike@virata-stone.com>
2013-09-21 07:44:09 -0700

    Just get it to work at all... ugh.

--- strategy.rb
+++ strategy.rb
@@ -1,87 +1,4 @@
-#module DalekSec
-#  attr_accessor :dalek_mode
-#
-#  def pick_mode!
-#    if desperate?
-#      self.dalek_mode = :panic
-#    elsif low_ammo?
-#      self.dalek_mode = :reload
-#    else
-#      self.dalek_mode = :exterminate
-#    end
-#  end
-#
-#  def mode_done?
-#    case dalek_mode
-#    when :panic
-#      false
-#    when :exterminate
-#      desperate? || low_ammo?
-#    when :reload
-#      desperate? || my.ammo_full?
-#    else
-#      true
-#    end
-#  end
-#
-#  def dalek_mode_turn!
-#    case dalek_mode
-#    when :panic
-#      panic_mode!
-#    when :exterminate
-#      exterminate_mode!
-#    when :reload
-#      reload_mode!
-#    end
-#  end
-#
-#  def dalek_turn
-#    pick_mode! if mode_done?
-#    dalek_mode_turn!
-#  end
-#
-#  def enemy
-#    opponents.first
-#  end
-#
-#  def desperate?
-#    my.armor <= 3
-#  end
-#
-#  def low_ammo?
-#    my.ammo <= 3
-#  end
-#
-#  def panic_mode!
-#    if my.ammo == 0
-#      rest
-#    elsif obscured? enemy
-#      move_towards! enemy
-#    elsif can_fire_at? enemy
-#      fire! 0
-#    else
-#      aim_at! enemy
-#    end
-#  end
-#
-#  def exterminate_mode!
-#    if obscured? enemy
-#      move_towards! enemy
-#    elsif can_fire_at? enemy
-#      fire! 0
-#    else
-#      aim_at! enemy
-#    end
-#  end
-#
-#  def reload_mode!
-#    rest
-#  end
-#end
-
 module DalekSec
-  attr_accessor :dalek_mode
-
   def dance
     first_possible_move %w(n s e w).shuffle
   end
@@ -104,15 +21,6 @@ module DalekSec
   end
 
   def dalek_turn
-    case dalek_mode
-    when :exterminate
-      exterminate_mode!
-    when :reload
-      reload_mode!
-    end
-  end
-
-  def exeterminate_mode!
     if my.ammo == 0
       rest
     elsif !enemy
@@ -125,19 +33,9 @@ module DalekSec
       aim_at! enemy
     end
   end
-
-  def reload_mode!
-    if my.ammo_full?
-      self.dalek_mode = :exterminate
-      exterminate_mode!
-    else
-      rest
-    end
-  end
 end
 
 include DalekSec
-self.dalek_mode = :exterminate
 
 on_turn do
   dalek_turn
